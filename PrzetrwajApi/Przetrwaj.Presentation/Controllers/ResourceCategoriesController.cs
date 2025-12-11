10ace1e5bb83f15667cbca7558370fda0ec13648
Patryk Niesporek <pn312381@student.polsl.pl>
2025-12-11 15:55:39 +0100

    Now add swagger docs

--- PrzetrwajApi/Przetrwaj.Presentation/Controllers/ResourceCategoriesController.cs
+++ PrzetrwajApi/Przetrwaj.Presentation/Controllers/ResourceCategoriesController.cs
@@ -1,8 +1,10 @@
 ﻿using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Przetrwaj.Application.Commands.Categories;
 using Przetrwaj.Application.Dtos;
 using Przetrwaj.Application.Quaries.Categories;
+using Przetrwaj.Domain;
 
 namespace Przetrwaj.Presentation.Controllers;
 
@@ -14,8 +16,10 @@ public class ResourceCategoriesController : ControllerBase
 	public ResourceCategoriesController(IMediator mediator) => _mediator = mediator;
 
 	[HttpPost]
+	[Authorize(UserRoles.Moderator)]
 	public async Task<ActionResult<CategoryDto>> Create([FromBody] CreateResourceCategoryCommand cmd, CancellationToken ct)
 	{
+		if (!ModelState.IsValid) return BadRequest(ModelState);
 		var created = await _mediator.Send(cmd, ct);
 		return CreatedAtAction(nameof(GetById), new { id = created.IdCategory }, created);
 	}
@@ -35,6 +39,7 @@ public class ResourceCategoriesController : ControllerBase
 	}
 
 	[HttpDelete("{id:int}")]
+	[Authorize(UserRoles.Moderator)]
 	public async Task<IActionResult> Delete(int id, CancellationToken ct)
 	{
 		var ok = await _mediator.Send(new DeleteResourceCategoryCommand { IdCategory = id }, ct);

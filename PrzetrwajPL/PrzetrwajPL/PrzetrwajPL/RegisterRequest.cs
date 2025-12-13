9bf49a42c5d6ac6f60df30d949454ee21dccd2e1
Patryk Niesporek <pn312381@student.polsl.pl>
2025-12-13 23:09:53 +0100

    added Password confirmation with error message. now register shows error from the server

--- PrzetrwajPL/PrzetrwajPL/PrzetrwajPL/RegisterRequest.cs
+++ PrzetrwajPL/PrzetrwajPL/PrzetrwajPL/RegisterRequest.cs
@@ -2,22 +2,27 @@
 
 namespace PrzetrwajPL
 {
-    public class RegisterRequest
-    {
-        [Required(ErrorMessage = "Imie jest wymagane")]
-        public string Name { get; set; }
+	public class RegisterRequest
+	{
+		[Required(ErrorMessage = "Imie jest wymagane")]
+		public string Name { get; set; }
 
-        [Required(ErrorMessage = "Nazwisko jest wymagane")]
-        public string Surname { get; set; }
+		[Required(ErrorMessage = "Nazwisko jest wymagane")]
+		public string Surname { get; set; }
 
-        [Required(ErrorMessage = "Email jest wymagany")]
-        public string Email { get; set; }
+		[Required(ErrorMessage = "Email jest wymagany")]
+		[EmailAddress(ErrorMessage = "Niepoprawny format email")]
+		public string Email { get; set; }
 
-        [Required(ErrorMessage = "Hasło jest wymagane")]
-        public string Password { get; set; }
+		[Required(ErrorMessage = "Hasło jest wymagane")]
+		public string Password { get; set; }
 
-        public string Role { get; set; }
+		[Required(ErrorMessage = "Hasło jest wymagane")]
+		[Compare(otherProperty: nameof(Password), ErrorMessage = "Hasła nie są takie same !")]
+		public string ConfirmPassword { get; set; }
 
-        public string Region { get; set; }
-    }
+		public string Role { get; set; }
+
+		public string Region { get; set; }
+	}
 }

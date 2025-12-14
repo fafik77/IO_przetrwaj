cdd16f18558e742ab2c5b6a4ae9d2f65faf5e289
Patryk Niesporek <pn312381@student.polsl.pl>
2025-12-14 20:44:12 +0100

    Statistics Controller proposed

---
+++ PrzetrwajApi/Przetrwaj.Application/Dtos/StatisticsDto.cs
@@ -0,0 +1,10 @@
+﻿namespace Przetrwaj.Application.Dtos;
+
+public class StatisticsDto
+{
+	public Int64 Regions { get; set; }
+	public Int64 Users { get; set; }
+	public Int64 ActiveDangers { get; set; }
+	public Int64 ActiveResources { get; set; }
+	public Int64 Moderators { get; set; }
+}

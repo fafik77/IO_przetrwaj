2b0b88ca90f4633f9d6ff183c495ce403464c32d
Patryk Niesporek <pn312381@student.polsl.pl>
2025-12-27 22:46:14 +0100

    applied migrations to Moderator and statistics indexes

--- PrzetrwajApi/Przetrwaj.Infrastucture/Migrations/20251227214425_ModeratorInfo.Designer.cs
+++ PrzetrwajApi/Przetrwaj.Infrastucture/Migrations/20251227214425_ModeratorInfo.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 using Przetrwaj.Infrastucture.Context;
@@ -11,9 +12,11 @@ using Przetrwaj.Infrastucture.Context;
 namespace Przetrwaj.Infrastucture.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251227214425_ModeratorInfo")]
+    partial class ModeratorInfo
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder

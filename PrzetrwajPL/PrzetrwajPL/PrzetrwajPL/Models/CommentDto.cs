fb842a427d948b2253517fa9e4e53fdc5c58f7ea
Dominika <domi.zak123@gmail.com>
2026-01-03 21:20:26 +0100

    Post details but adding comments and votes doesn't work

--- PrzetrwajPL/PrzetrwajPL/PrzetrwajPL/Models/CommentDto.cs
+++ PrzetrwajPL/PrzetrwajPL/PrzetrwajPL/Models/CommentDto.cs
@@ -5,5 +5,5 @@ public class CommentDto
 	//there is no point in including Id, or Post info (as we already know the post)
 	public required string Comment { get; set; }
 	public DateTimeOffset DateCreated { get; set; }
-	public UserGeneralDto? Autor { get; set; }
+	public UserGeneralDto? Author { get; set; }
 }

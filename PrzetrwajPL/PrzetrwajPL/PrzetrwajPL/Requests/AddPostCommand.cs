75de46dcba4c6ece2a5480007691ebd8d12a479b
kkubi <kkubi@DELL_16_KAROL>
2026-01-16 15:23:15 +0100

    dodany AddPostCommand, dodany Formularz dla posta

---
+++ PrzetrwajPL/PrzetrwajPL/PrzetrwajPL/Requests/AddPostCommand.cs
@@ -0,0 +1,10 @@
+﻿namespace PrzetrwajPL.Requests;
+
+public class AddPostCommand
+{
+    public string Title { get; set; } = "";
+    public string? Description { get; set; }
+    public int IdCategory { get; set; }
+    public string? CustomCategory { get; set; }
+    public int IdRegion { get; set; }
+}

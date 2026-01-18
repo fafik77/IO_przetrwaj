4fe6950fe54a573faa177c2741f47a48afd72a5e
Patryk Niesporek <pn312381@student.polsl.pl>
2026-01-18 12:43:22 +0100

    modified cors

--- PrzetrwajApi/Przetrwaj.Api/Program.cs
+++ PrzetrwajApi/Przetrwaj.Api/Program.cs
@@ -34,15 +34,23 @@ builder.Services.AddProblemDetails();
 
 #region CORS Access-Control-Allow-Origin
 var AllowAllOrigins = "_AllowAllOrigins";
+var AllowPrzetrwajOrigins = "_AllowPrzetrwajOrigins";
 builder.Services.AddCors(options =>
 {
-	options.AddPolicy(name: AllowAllOrigins,
-					  policy =>
-					  {
-						  policy.AllowAnyOrigin()
-						  .AllowAnyHeader()
-						  .AllowAnyMethod();
-					  });
+	options.AddPolicy(name: AllowPrzetrwajOrigins,
+		policy =>
+		{
+			policy.
+			WithOrigins(
+				"https://localhost:7173",
+				"https://localhost",
+				"https://przetrwaj-front.grayflower-7f624026.polandcentral.azurecontainerapps.io"
+			)
+			.AllowAnyHeader()
+			.AllowAnyMethod()
+			.AllowCredentials()
+			;
+		});
 });
 #endregion
 

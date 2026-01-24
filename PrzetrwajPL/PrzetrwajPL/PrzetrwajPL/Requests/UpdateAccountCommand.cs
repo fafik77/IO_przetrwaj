eed87891f6b5751b324df59befe690f42c8c6d5a
Dominika <domi.zak123@gmail.com>
2026-01-24 15:20:02 +0100

    user settings

--- PrzetrwajPL/PrzetrwajPL/PrzetrwajPL/Requests/UpdateAccountCommand.cs
+++ PrzetrwajPL/PrzetrwajPL/PrzetrwajPL/Requests/UpdateAccountCommand.cs
@@ -8,7 +8,11 @@ public class UpdateAccountCommand
 	public string? Name { get; set; }
 	[MaxLength(64)]
 	public string? Surname { get; set; }
-	[EmailAddress]
-	public string? Email { get; set; }
+    [EmailAddress(ErrorMessage = "Niepoprawny format email")]
+    public string? Email { get; set; }
 	public int? IdRegion { get; set; }
+    public string NewPassword { get; set; }
+
+    [Required(ErrorMessage = "Hasło jest wymagane")]
+    public string OldPassword { get; set; }
 }

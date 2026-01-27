290e55c85c125ce1f8d7471b3e9f1a59492c3212
Patryk Niesporek <pn312381@student.polsl.pl>
2026-01-27 16:51:40 +0100

    auto user region, settings

--- PrzetrwajPL/PrzetrwajPL/PrzetrwajPL/Components/Controllers/AccountController.cs
+++ PrzetrwajPL/PrzetrwajPL/PrzetrwajPL/Components/Controllers/AccountController.cs
@@ -1,20 +1,23 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Mvc;
+using PrzetrwajPL.Models;
 using System.IdentityModel.Tokens.Jwt;
+using System.Net;
 using System.Security.Claims;
-using System.Xml.Linq;
 
 namespace PrzetrwajPL.Components.Controllers;
 
 [Route("account")]
 public class AccountController : Controller
 {
-	private readonly IHttpContextAccessor _httpContextAccessor;
+	private readonly IHttpContextAccessor _ContextAccessor;
+	private readonly IHttpClientFactory _ClientFactory;
 
-	public AccountController(IHttpContextAccessor httpContextAccessor)
+	public AccountController(IHttpContextAccessor httpContextAccessor, IHttpClientFactory httpClientFactory)
 	{
-		_httpContextAccessor = httpContextAccessor;
+		_ContextAccessor = httpContextAccessor;
+		_ClientFactory = httpClientFactory;
 	}
 
 	[HttpGet("signin")]
@@ -78,7 +81,7 @@ public class AccountController : Controller
 		{
 			new AuthenticationToken { Name = "access_token", Value = token }
 		});
-		await _httpContextAccessor.HttpContext.SignInAsync(
+		await _ContextAccessor.HttpContext.SignInAsync(
 			CookieAuthenticationDefaults.AuthenticationScheme,
 			principal,
 			authProperties);
@@ -89,5 +92,37 @@ public class AccountController : Controller
 		//		ExpiresUtc = jwtToken.ValidTo
 		//	});
 		return Results.LocalRedirect("/");
+	}
+
+	[HttpGet("refresh-cookie")]
+	public async Task<IResult> RefreshCookie(string redirectTo = "")
+	{
+		redirectTo = redirectTo.Trim();
+		if (!redirectTo.StartsWith("/")) redirectTo = "/" + redirectTo;
+
+		var client = _ClientFactory.CreateClient("ServerAPI"); 
+		var user = await client.GetFromJsonAsync<UserWithPersonalDataDto>("/Account");
+
+		var claims = new List<Claim>
+		{
+			new Claim(ClaimTypes.NameIdentifier, user.Id),
+			new Claim(ClaimTypes.Name, user.Name ?? user.Email!),
+			new Claim(ClaimTypes.Email, user.Email!),
+			new Claim("Region", user.Region?.Id.ToString() ?? "0"),
+			new Claim("Surname", user.Surname ?? ""),
+			new Claim("Name", user.Name ?? ""),
+		};
+		foreach (var role in user.Roles)
+		{
+			claims.Add(new Claim(ClaimTypes.Role, role));
+		}
+		var identity = new ClaimsIdentity(claims, CookieAuthenticationDefaults.AuthenticationScheme);
+		var principal = new ClaimsPrincipal(identity);
+		await _ContextAccessor.HttpContext.SignInAsync(
+			CookieAuthenticationDefaults.AuthenticationScheme,
+			principal,
+			new AuthenticationProperties { IsPersistent = true });
+		var succStr = "Zmiany zapisane pomyślnie";
+		return Results.LocalRedirect(redirectTo+$"?success={WebUtility.UrlEncode(succStr)}");
 	}
 }

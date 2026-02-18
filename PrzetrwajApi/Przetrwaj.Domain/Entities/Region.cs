845d11b8e3a5ff4d380edc68312cdf3fb2a809c3
Patryk Niesporek <pn312381@student.polsl.pl>
2026-02-18 13:23:25 +0100

    Region using composite key

--- PrzetrwajApi/Przetrwaj.Domain/Entities/Region.cs
+++ PrzetrwajApi/Przetrwaj.Domain/Entities/Region.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Przetrwaj.Domain.Models.Dtos;
+using System.ComponentModel.DataAnnotations;
 
 namespace Przetrwaj.Domain.Entities;
 
@@ -8,11 +9,93 @@ public class Region
 	public int IdRegion { get; set; }
 	//[MaxLength(100)]
 	public required string Name { get; set; }
-	public double Lat {  get; set; }
+	public double Lat { get; set; }
 	public double Long { get; set; }
 
 
 	public virtual ICollection<Post> Posts { get; set; } = new List<Post>();
 
 	public virtual ICollection<AppUser> Users { get; set; } = new List<AppUser>();
+}
+
+
+public interface IRegionInfo
+{
+	public int Id { get; }
+	public string Name { get; }
+	public LatLong? LatLong { get; }
+}
+//source TERYT: TERC_Urzedowy, ULIC_Urzedowy
+//Województwo || Polska{id=0}
+public class RegionWoj : IRegionInfo
+{
+	//2x Char
+	[Key]
+	public short Id { get; set; }
+	public required string Name { get; set; }
+
+	public virtual ICollection<Post> Posts { get; set; } = new List<Post>();
+	public virtual ICollection<RegionPow> Powiaty { get; set; } = [];
+
+	int IRegionInfo.Id => Id * 100000;
+	LatLong? IRegionInfo.LatLong => null;
+}
+//Powiat
+public class RegionPow : IRegionInfo
+{
+	//2x Char
+	[Key]
+	public short WojId { get; set; }
+	//2x Char
+	[Key]
+	public short Id { get; set; }
+	public required string Name { get; set; }
+	public double Lat { get; set; }
+	public double Long { get; set; }
+
+	public virtual required RegionWoj Woj { get; set; }
+	public virtual ICollection<Post> Posts { get; set; } = new List<Post>();
+	public virtual ICollection<AppUser> Users { get; set; } = new List<AppUser>();
+	public virtual ICollection<RegionGmi> Gminy { get; set; } = [];
+	public virtual ICollection<RegionUl> Ulice { get; set; } = [];
+
+	int IRegionInfo.Id => WojId * 100000 + Id * 1000;
+	LatLong IRegionInfo.LatLong => new LatLong(Lat, Long);
+}
+//Gmina/Dzielnica
+public class RegionGmi : IRegionInfo
+{
+	//2x Char
+	[Key]
+	public short WojId { get; set; }
+	//2x Char
+	[Key]
+	public short PowId { get; set; }
+	//2+1x Char (where last one will be ignored as it violates the Name uniqness)
+	[Key]
+	public short Id { get; set; }
+	public required string Name { get; set; }
+	public double Lat { get; set; }
+	public double Long { get; set; }
+
+	public virtual required RegionPow Pow { get; set; }
+	public virtual ICollection<Post> Posts { get; set; } = new List<Post>();
+
+	int IRegionInfo.Id => WojId * 100000 + PowId * 1000 + Id * 10;
+	LatLong IRegionInfo.LatLong => new LatLong(Lat, Long);
+}
+//Ulica
+public class RegionUl
+{
+	[Key]
+	public short WojId { get; set; }
+	[Key]
+	public short PowId { get; set; }
+	//SYM_UL
+	[Key]
+	public int Id { get; set; }
+	public required string Name { get; set; }
+
+	public virtual required RegionPow Pow { get; set; }
+	public virtual ICollection<Post> Posts { get; set; } = new List<Post>();
 }
---
+++ PrzetrwajApi/Przetrwaj.Domain/Models/Dtos/LatLong.cs
@@ -0,0 +1,27 @@
+﻿using Przetrwaj.Domain.Entities;
+
+namespace Przetrwaj.Domain.Models.Dtos;
+
+public class LatLong(double Lat, double Long)
+{
+	public double Lat { get; set; } = Lat;
+	public double Long { get; set; } = Long;
+}
+
+public class RegionDto
+{
+	public int Id { get; set; }
+	public required string Name { get; set; }
+	public LatLong? LatLong { get; set; }
+
+
+	public static RegionDto? Map(IRegionInfo region)
+	{
+		return region is null ? null : new RegionDto
+		{
+			Id = region.Id,
+			Name = region.Name,
+			LatLong = region.LatLong,
+		};
+	}
+}

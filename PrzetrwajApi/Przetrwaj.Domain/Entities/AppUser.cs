113b89846e93c3bae1c95a4299dc4b5ae72af88b
Patryk Niesporek <pn312381@student.polsl.pl>
2026-02-21 18:11:09 +0100

    proposed changes to DB struct and acompanied changes in API

--- PrzetrwajApi/Przetrwaj.Domain/Entities/AppUser.cs
+++ PrzetrwajApi/Przetrwaj.Domain/Entities/AppUser.cs
@@ -8,20 +8,30 @@ public class AppUser : IdentityUser
 {
 	public AppUser() : base() { }
 
-	[MaxLength(24)]
+	//[MaxLength(24)]
 	public string? Name { get; set; }
-	[MaxLength(24)]
+	//[MaxLength(24)]
 	public string? Surname { get; set; }
 	public int IdRegion { get; set; }
 	public bool Banned { get; set; } = false;
 	public DateTimeOffset? BanDate { get; set; }
 	//[MaxLength(300)]
 	public string? BanReason { get; set; }
-	[MaxLength(36)]
+	//[MaxLength(36)]
 	public string? BannedById { get; set; }
 	public DateTimeOffset RegistrationDate { get; set; } = DateTimeOffset.UtcNow;
 	public bool ModeratorRolePending { get; set; }
 	public DateTimeOffset? ModeratorSince { get; set; }
+
+	/// <summary>
+	/// Publicly visible info
+	/// </summary>
+	//public short PowiatId { get; set; }
+	/// <summary>
+	/// private preference for Post display sorting rules
+	/// </summary>
+	//public short GminaId { get; set; } = 0;
+	//public bool z_sight, z_hearing, z_movement, z_speach, z_body;
 
 
 	[ForeignKey(nameof(IdRegion))]
--- PrzetrwajApi/Przetrwaj.Domain/Entities/Post.cs
+++ PrzetrwajApi/Przetrwaj.Domain/Entities/Post.cs
@@ -21,6 +21,10 @@ public class Post
 	public string CustomCategory { get; set; } = string.Empty;
 
 	public int IdRegion { get; set; }
+	// double Lat,Long // more exact location for the Post: `Polska 2.Woj 2.Powiat 2.(Gmina 3).adres ulicy` TERYT, TERC
+	//public short? IdWojOnly {  get; set; }
+	//public short? IdPowOnly {  get; set; }
+	//public int? IdGmiOnly {  get; set; }
 
 	[MaxLength(36)]
 	public required string IdAutor { get; set; }

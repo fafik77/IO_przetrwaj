fd63a993f2e8b0ca0de8ba4ad35b923b7325419f
Patryk Niesporek <pn312381@student.polsl.pl>
2026-06-28 15:26:53 +0200

    updated AddPostCommand

--- PrzetrwajPL/PrzetrwajPL/Przetrwaj.CommonLibrary/Requests/AddPostCommand.cs
+++ PrzetrwajPL/PrzetrwajPL/Przetrwaj.CommonLibrary/Requests/AddPostCommand.cs
@@ -1,18 +1,29 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Przetrwaj.CommonLibrary.Models;
+using System.ComponentModel.DataAnnotations;
 
 namespace Przetrwaj.CommonLibrary.Requests;
 
 public class AddPostCommand
 {
 	[Required(ErrorMessage = "Tytuł jest wymagany")]
-	[MaxLength(250, ErrorMessage = "Tytuł nie może być dłuższy niż 250 znaków")]
+	[Length(3, 200, ErrorMessage = "Tytuł musi mieć od 3 do 200 znaków")]
 	public required string Title { get; set; } = "";
-	[MaxLength(2500, ErrorMessage = "Opis nie może być dłuższy niż 250, znaków")]
+
+	[MaxLength(2000, ErrorMessage = "Opis nie może być dłuższy niż 2'000 znaków")]
 	public string? Description { get; set; }
+
 	[Range(1, int.MaxValue, ErrorMessage = "Kategoria jest wymagana")]
 	public int IdCategory { get; set; }
-	[Length(3, 60, ErrorMessage = "Własna kategoria musi mieć od 3 do 60 znaków")]
+
+	[Length(3, 100, ErrorMessage = "Własna kategoria musi mieć od 3 do 100 znaków")]
 	public string? CustomCategory { get; set; }
-	[Range(0, int.MaxValue, ErrorMessage = "Region jest wymagany")]
-	public int IdRegion { get; set; }
+
+	[Required(ErrorMessage = "Lokalizacja zdarzenia jest wymagana")]
+	public LatLong LatLong { get; set; }
+
+
+	public RegionPrecision RegionPrecision { get; set; } = RegionPrecision.GMI;
+
+
+	public AddAttachments? Attachments { get; set; }
 }

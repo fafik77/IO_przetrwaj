088b9b0528e3bb66aacbb251c7c9f1cc6c1cc5f9
Patryk Niesporek <pn312381@student.polsl.pl>
2026-06-29 18:33:11 +0200

    added ImpedimentsPanel for moderator

---
+++ PrzetrwajPL/PrzetrwajPL/Przetrwaj.CommonLibrary/Requests/EditImpediment.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Przetrwaj.CommonLibrary.Requests;
+
+public class EditImpediment
+{
+	[Required]
+	[Range(0, 31)]
+	public short Id { get; set; }
+	[Required]
+	[Length(3, 100)]
+	public required string Name { get; set; }
+}

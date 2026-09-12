cc4df22f731c5e6903c3bff91433442ff33f14eb
fafik77 <fafik77>
2026-09-12 14:35:24 +0200

    and author id

--- PrzetrwajApi/Przetrwaj.Domain/Models/Dtos/CommentDto.cs
+++ PrzetrwajApi/Przetrwaj.Domain/Models/Dtos/CommentDto.cs
@@ -5,6 +5,7 @@ namespace Przetrwaj.Domain.Models.Dtos;
 public record CommentDto
 {
     public required string CommentId { get; set; }
+    public required string AuthorId { get; set; }
     public required string Comment { get; set; }
     public DateTimeOffset DateCreated { get; set; }
     public UserGeneralDtoNoRegion? Author { get; set; }
@@ -15,6 +16,7 @@ public record CommentDto
         return new CommentDto
         {
             CommentId = comment.IdComment,
+            AuthorId = comment.IdAutor,
             Comment = comment.Comment,
             DateCreated = comment.DateCreated,
             Author = UserGeneralDtoNoRegion.Map(comment.IdAutorNavigation),

7bcd209425418c21d4f426cba142ec36c641b8ab
fafik77 <fafik77>
2026-09-19 13:44:42 +0200

    included max len of Ban Reason

--- PrzetrwajApi/Przetrwaj.Application/Commands/Users/BanUserCommand.cs
+++ PrzetrwajApi/Przetrwaj.Application/Commands/Users/BanUserCommand.cs
@@ -7,5 +7,6 @@ public class BanUserCommand
 	[Required]
 	public required string UserIdOrEmail { get; set; }
 	[Required]
-	public required string Reason { get; set; }
+    [MaxLength(500)]
+    public required string Reason { get; set; }
 }
